c321fd166775601b5b3854c0cd4e39459009fc01
AHMAD RIAN <112596695+ahmad-rian@users.noreply.github.com>
2024-07-12 10:57:23 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://tokolatif.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
